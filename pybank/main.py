2b6a721118b50e64916750db4ee1f61aa79b2176
KATE A <153536330+ktaytg@users.noreply.github.com>
2024-01-15 13:34:00 -0500

    Update main.py

--- pybank/main.py
+++ pybank/main.py
@@ -1,1 +1,7 @@
+#Your task is to create a Python script that analyzes the records to calculate each of the following values:
+#The total number of months included in the dataset
 
+#The net total amount of "Profit/Losses" over the entire period
+#The changes in "Profit/Losses" over the entire period, and then the average of those changes
+#The greatest increase in profits (date and amount) over the entire period
+#The greatest decrease in profits (date and amount) over the entire period
